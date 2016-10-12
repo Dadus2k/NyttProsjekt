65d655895e69582c78c9b5ab120b753daeb5f5aa
Daniel <Daniel@Dadus2kMaster>
2016-10-12 15:18:06 +0200

    Revert HD

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,7 +8,5 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MainClass(), config);
-		config.width = 1920;
-		config.height = 1080;
 	}
 }
