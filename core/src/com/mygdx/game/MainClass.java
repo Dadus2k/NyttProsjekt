9ef58300ef5dc1ed3371786924335c123154a8a6
Daniel <Daniel@Dadus2kMaster>
2016-10-12 15:47:40 +0200

    Hotfix

--- core/src/com/mygdx/game/MainClass.java
+++ core/src/com/mygdx/game/MainClass.java
@@ -10,7 +10,6 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MainClass extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	float x,y;
 	
 	@Override
 	public void create () {
@@ -22,25 +21,8 @@ public class MainClass extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		
-		if(Gdx.input.isKeyPressed(Keys.UP)){
-			y = y+ 4;
-		}
-		if(Gdx.input.isKeyPressed(Keys.DOWN)){
-			y = y- 4;
-		}
-		
-		if(Gdx.input.isKeyPressed(Keys.RIGHT)){
-			x = x+ 4;
-		}
-		if(Gdx.input.isKeyPressed(Keys.LEFT)){
-			x = x- 4;
-		}
-	
-		
 		batch.begin();
-		batch.draw(img, x, y);
+		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
--- core/src/com/mygdx/game/Test1.java
+++ core/src/com/mygdx/game/Test1.java
@@ -1,9 +1,52 @@
 package com.mygdx.game;
 
-public class Test1 {
-	private void syso() {
-		// TODO Auto-generated method stub
-		syso();
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+public class Test1 extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	float x,y;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		
+		if(Gdx.input.isKeyPressed(Keys.UP)){
+			y = y+ 4;
+		}
+		if(Gdx.input.isKeyPressed(Keys.DOWN)){
+			y = y- 4;
+		}
+		
+		if(Gdx.input.isKeyPressed(Keys.RIGHT)){
+			x = x+ 4;
+		}
+		if(Gdx.input.isKeyPressed(Keys.LEFT)){
+			x = x- 4;
+		}
+	
+		
+		batch.begin();
+		batch.draw(img, x, y);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
 	}
 }
