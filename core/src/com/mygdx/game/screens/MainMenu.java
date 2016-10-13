2cee36b8519bf189f4b3f0a9480673903570e7ab
petterpinjata <markus_medalen@hotmail.com>
2016-10-13 19:25:08 +0200

    glemte denne

--- core/src/com/mygdx/game/screens/MainMenu.java
+++ core/src/com/mygdx/game/screens/MainMenu.java
@@ -3,16 +3,20 @@ package com.mygdx.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 
 public class MainMenu implements Screen {
 	
@@ -20,7 +24,7 @@ public class MainMenu implements Screen {
 	private Sprite menuPic;
 	private Music menuMusic;
 
-	private Stage stage; 
+	private Stage stage;
 	private Table table;
 	private TextButton buttonPlay, buttonExit;
 	private Label heading; 
@@ -31,31 +35,66 @@ public class MainMenu implements Screen {
 	@Override
 	public void show() {
 
+		stage = new Stage();
+	
+		Gdx.input.setInputProcessor(stage);
 		
 		//Menymusikk som looper
 		menuMusic = Gdx.audio.newMusic(Gdx.files.internal("AOE_mp3.mp3"));
 		menuMusic.setLooping(true);
 		menuMusic.play();
 		
+		
 		//Det grafiske 
-		atlas = new TextureAtlas("ui/button.pack");
+		atlas = new TextureAtlas("ui//button.pack");
 		skin = new Skin(atlas);
 		
+		table = new Table(skin);
+		table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
 		white = new BitmapFont(Gdx.files.internal("font/white.fnt"), false);
+
+		TextButtonStyle textButtonStyle = new TextButtonStyle();
+		textButtonStyle.up = skin.getDrawable("button.up");		
+		textButtonStyle.down = skin.getDrawable("button.donw");	//Jeg skrev down feil LEL
+		textButtonStyle.pressedOffsetX = 1;
+		textButtonStyle.pressedOffsetY = -1;
+		textButtonStyle.font = white;
+		textButtonStyle.fontColor = Color.BLACK; //Dette gjøre for å få sort skrift i stede for hvit. Kan fjernes om hvit farge ønskes
+		
+		buttonExit = new TextButton("EXIT", textButtonStyle);
+		buttonExit.pad(20);
+		
+		table.add(buttonExit);
+		table.debug(); //TODO fjent når ferdig med implementasjoene
+		stage.addActor(table);
+		
+		
 		
 		batch = new SpriteBatch();
 		Texture splashTexture = new Texture("mab_meme.png");
 		menuPic = new Sprite(splashTexture);
 		menuPic.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
+//		batch.begin();
+//		batch.draw(menuPic,0,0);
+//		batch.end();
+//		
 	}
 
 	@Override
 	public void render(float delta) {
+		
+//		Gdx.gl.glClearColor(0, 0, 0, 1);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+			
 		batch.begin();
 		batch.draw(menuPic,0,0);
 		batch.end();
 
+		stage.act(delta);
+		stage.draw(); //
+		
 	}
 
 	@Override
